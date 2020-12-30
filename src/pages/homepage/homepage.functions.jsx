6291319439c911aeeb1bea9182c18e78ee15a26a
xRomasik <xromasikx@gmail.com>
2020-12-30 22:52:22 +0100

    Adjusted fetch function

--- src/pages/homepage/homepage.functions.jsx
+++ src/pages/homepage/homepage.functions.jsx
@@ -4,7 +4,6 @@ const timeConvertTo24hLocalTimeZoneFormat = (fetchedTime, timeOffset) => {
     const timeOffsetMinutes = timeOffset % 60;
     const timeOffsetHours = (timeOffset - timeOffsetMinutes) / 60;
 
-    console.log(hours, minutes, modifier, timeOffsetHours)
     if (hours === '12') {
         hours = '00';
     };
@@ -16,7 +15,6 @@ const timeConvertTo24hLocalTimeZoneFormat = (fetchedTime, timeOffset) => {
     hours = parseInt(hours) + timeOffsetHours;
     minutes = parseInt(minutes) + timeOffsetMinutes;
 
-    console.log(hours, minutes, modifier, timeOffsetHours)
     if (minutes >= 60) {
         minutes = minutes - 60;
         hours = hours + 1
@@ -39,39 +37,39 @@ const timeConvertTo24hLocalTimeZoneFormat = (fetchedTime, timeOffset) => {
 
 export const getSunriseSunsetTimes = (country, date, API_KEY, API_KEY2, setSunsetSunriseTime, setErrorMessage) => {
     if (country.length > 2 && country.match(/^[^0-9]*$/)) {
+
+        let coordinates = {}
+
         fetch(`https://api.opencagedata.com/geocode/v1/json?q=${country.toLowerCase()}&key=${API_KEY}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data)
-                let coordinates = ({
+                coordinates = ({
                     latitude: data.results[0].geometry.lat,
                     longitude: data.results[0].geometry.lng,
                     timezone: data.results[0].annotations.timezone.name
                 })
-                fetch(`https://timezoneapi.io/api/timezone/?${coordinates.timezone}&date=${date}&token=${API_KEY2}`)
-                    .then(response => response.json())
-                    .then(data => {
-                        coordinates = ({
-                            ...coordinates,
-                            timeOffset: parseInt(data.data.datetime.offset_minutes)
-                        })
-                        console.log(coordinates)
-                        fetch(`https://api.sunrise-sunset.org/json?lat=${coordinates.latitude}&lng=${coordinates.longitude}&date=${date}`)
-
-                            .then(response => response.json())
-                            .then(data => {
-                                console.log(data)
-                                setSunsetSunriseTime({
-                                    sunriseTime: timeConvertTo24hLocalTimeZoneFormat(data.results.sunrise, coordinates.timeOffset),
-                                    sunsetTime: timeConvertTo24hLocalTimeZoneFormat(data.results.sunset, coordinates.timeOffset)
-                                });
-                                setErrorMessage(null)
-                            }).catch(() => setErrorMessage('Something went wrong, please check the name of the country and try again.'))
-                    }).catch(() => setErrorMessage('Something went wrong, please check the name of the country and try again.'))
+            })
+            .then(() => fetch(`https://timezoneapi.io/api/timezone/?${coordinates.timezone}&date=${date}&token=${API_KEY2}`))
+            .then(response => response.json())
+            .then(data => {
+                coordinates = ({
+                    ...coordinates,
+                    timeOffset: parseInt(data.data.datetime.offset_minutes)
+                })
+            })
+            .then(() => fetch(`https://api.sunrise-sunset.org/json?lat=${coordinates.latitude}&lng=${coordinates.longitude}&date=${date}`))
+            .then(response => response.json())
+            .then(data => {
+                setSunsetSunriseTime({
+                    sunriseTime: timeConvertTo24hLocalTimeZoneFormat(data.results.sunrise, coordinates.timeOffset),
+                    sunsetTime: timeConvertTo24hLocalTimeZoneFormat(data.results.sunset, coordinates.timeOffset)
+                });
+                setErrorMessage(null)
 
             }).catch(() => setErrorMessage('Something went wrong, please check the name of the country and try again.'))
+    }
 
-    } else if (!country.match(/^[^0-9]*$/)) {
+    else if (!country.match(/^[^0-9]*$/)) {
         setErrorMessage('It seems like there is a number in your country name.')
     } else {
         setErrorMessage('Country name must be at least 3 letters long.')
